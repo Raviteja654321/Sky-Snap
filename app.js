a2cf59f73e9b653634b3a5afe1be27e2f72d6da0
Raviteja654321 <94986564+Raviteja654321@users.noreply.github.com>
2023-06-08 00:53:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ const app = express();
 
 app.use(bodyparser.urlencoded({ extended: true }));
 
+app.use(express.static(__dirname+"/public"));
+
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.post('/index.html', function (req, res) {
+app.post("/index.html", function (req, res) {
     const city = req.body.cityName;
     console.log("Weather is requested at "+city);
     const apikey = "a23795ac9e0075261667c58602b65274";
@@ -26,16 +28,24 @@ app.post('/index.html', function (req, res) {
             const temperature = weatherData.main.temp;
             const weatherDescription = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
+            const latitude = weatherData.coord.lat;
+            const longitude = weatherData.coord.lon;  
+            const humidity = weatherData.main.humidity;
+            const windSpeed = weatherData.wind.speed;
+            const pressure = weatherData.main.pressure;
+
             const imageurl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-            res.write("<p>The weather is currently " + weatherDescription + "<p>");
-            res.write("<h1>The Temperature in " + city + " is currently " + temperature + " degrees Celcius <h1>");
+            res.write("<h1>The weather is currently " + weatherDescription + "<h1>");
             res.write("<img src=" + imageurl + " alt=weather_icon>");
+            res.write("<h1>The Temperature in " + city + " is currently " + temperature + " degrees Celcius <h1>");
+            res.write("<p>Latitude: " + latitude + " Longitude: " + longitude + "</p>");
+            res.write("<p>Humidity: " + humidity + "%</p>");
+            res.write("<p>Wind Speed: " + windSpeed + " m/s</p>");
+            res.write("<p>Pressure: " + pressure + " hPa</p>");
             res.send();
         })
     });
 })
-
-
 
 
 app.listen(3000, function () {
